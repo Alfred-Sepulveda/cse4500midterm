7c1267fbec2cebc2ff06e8fb01678abee8851543
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-25 13:13:50 -0700

    fix

--- database/migrations/2022_03_24_201200_create_users.php
+++ database/migrations/2022_03_24_201200_create_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('LastName');
             $table->string('FirstName');
             $table->string('Email');

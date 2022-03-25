d2d6fab9a7be4cd76a3dacc243868367ac28484c
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-24 19:49:49 -0700

    edit controllers

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class EquipmentCatagory extends Controller
+class EquipmentController extends Controller
 {
     /**
      * Display a listing of the resource.

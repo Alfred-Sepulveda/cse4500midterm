69c84fef26ca1b6ed7aa67f58164f7c056a43198
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-25 13:10:53 -0700

    changed tables

--- database/migrations/2022_03_24_201200_create_users.php
+++ database/migrations/2022_03_24_201200_create_users.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('FirstName');
             $table->string('Email');
             $table->integer('ContactNumber');
-            $table->increments('EmployeeID');
+            $table->integer('EmployeeID',true,true);
             $table->string('HistoryServices');
             $table->timestamps();
         });

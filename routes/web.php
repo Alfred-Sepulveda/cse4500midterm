ddadaef95451458f1afba29df646e0739884dfd2
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-25 19:06:24 -0700

    another change

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\TodoController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ManufacturerSaleController;
-use App\Http\Controllers\ManufacturerSupportController;
 use App\Http\Controllers\EquipmentController;
 use app\Http\Controllers\ManufacturerSupportController;  
 /*

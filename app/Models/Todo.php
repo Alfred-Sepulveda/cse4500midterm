109f1c6bffaf38ca6e3f0a419efbb41b4b5e72f4
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-30 14:36:01 -0700

    changed todo.php

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    protected $fillable = ['title','progress'];
+    protected $fillable = ['name','email','phonenumber',
+        'support','progress'];
     use HasFactory;
 }

2a8b2f07df10da2ee2f94552c998e80c15cd037b
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-03-29 19:11:45 -0700

    option edit

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -16,7 +16,6 @@
     <x-adminlte-select name="HistoyServices" label="Services Previously Done if any">
     <x-adminlte-options :options="['Desktop Repair'=>'Desktop Repair', 'Laptop Repair'=>'Laptop Repair','Phone Repair'=>'Phone Repair']"
         empty-option="Select a Repair Service"/>
-    </x-adminlte-select>
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
